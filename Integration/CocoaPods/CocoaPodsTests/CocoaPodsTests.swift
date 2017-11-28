486e385583f69d7f583e42eadea387b43cb5e1e2
Andre Stenvall <andre@stenvall.me>
2017-11-28 19:40:02 +0100

    Add integration test to CocoaPods example project

--- Integration/CocoaPods/CocoaPodsTests/CocoaPodsTests.swift
+++ Integration/CocoaPods/CocoaPodsTests/CocoaPodsTests.swift
@@ -7,13 +7,14 @@
 //
 
 import XCTest
+@testable import SnapshotTest
 @testable import CocoaPods
 
-class CocoaPodsTests: XCTestCase {
+class CocoaPodsTests: SnapshotTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+//        self.recordMode = true
     }
     
     override func tearDown() {
@@ -21,16 +22,10 @@ class CocoaPodsTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testViewSnapshot() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 200))
+        view.backgroundColor = .red
+
+        AssertSnapshot(view)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
